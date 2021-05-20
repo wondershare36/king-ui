888e7b481b37ac8594dcbe71c5b76c25c1b67754
siliwu <siliwu@tencent.com>
2021-05-20 13:23:39 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     Button示例
+    <h1>示例1</h1>
+    <div>
+      <Button>你好</Button>
+    </div>
   </div>
 </template>
 
 <script>
+import Button from '../lib/Button.vue';
 export default {
-name: "ButtonDemo"
+name: "ButtonDemo",
+  components: { Button }
 }
 </script>
 
