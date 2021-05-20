53b999e82a28848eb7e817533de5f35dbf4396d1
siliwu <siliwu@tencent.com>
2021-05-20 15:58:33 +0800

    支持level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,47 @@
 <template>
+  <div>Button示例</div>
   <div>
-    Button示例
-    <h1>示例1</h1>
+    <h2>示例1</h2>
     <div>
-      <Button  @click="onClick">你好</Button>
-      <Button theme="button" @click="onClick">你好</Button>
-      <Button theme="link" @click="onClick">你好</Button>
-      <Button theme="text" @click="onClick">你好</Button>
+      <Button theme="button">button</Button>
+      <Button theme="link">link</Button>
+      <Button theme="text">text</Button>
+    </div>
+  </div>
+  <div>
+    <h2>示例2</h2>
+    <div>
+      <Button size="big">big</Button>
+      <Button size="normal">normal</Button>
+      <Button size="small">small</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">big</Button>
+      <Button theme="link" size="normal">normal</Button>
+      <Button theme="link" size="small">small</Button>
+    </div>
+    <div>
+      <Button theme="text" size="big">big</Button>
+      <Button theme="text" size="normal">normal</Button>
+      <Button theme="text" size="small">small</Button>
+    </div>
+    <div>
+      <h2>示例3</h2>
+      <div>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="link" level="main">主要按钮</Button>
+        <Button theme="link">普通按钮</Button>
+        <Button theme="link" level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="text" level="main">主要按钮</Button>
+        <Button theme="text">普通按钮</Button>
+        <Button theme="text" level="danger">危险按钮</Button>
+      </div>
     </div>
   </div>
 </template>
@@ -17,11 +52,11 @@ import Button from '../lib/Button.vue';
 export default {
   name: 'ButtonDemo',
   components: {Button},
-  setup(){
-    const onClick=()=>{
+  setup() {
+    const onClick = () => {
       console.log('hi');
-    }
-    return {onClick}
+    };
+    return {onClick};
   },
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,30 +1,49 @@
 <template>
-  <button class="gulu-button" :class="`gulu-theme-${theme}`">
+  <button class="gulu-button" :class="classes">
     <slot/>
   </button>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
+
 export default {
   name: 'Button',
   props: {
-    theme:{
-      type:String,
-      default:'button'
+    theme: {
+      type: String,
+      default: 'button',
+    },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    level: {
+      type: String,
+      default: 'normal',
     }
   },
-  setup() {
-
+  setup(props) {
+    const {theme, size, level} = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
+      };
+    });
+    return {classes};
   }
 };
 </script>
 
-<style lang="scss" >
-$h:32px;
-$border-color:#d9d9d9;
-$color:#333;
+<style lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
 $blue: #40a9ff;
-$radius:4px;
+$radius: 4px;
+$red: #F56C6C;
 .gulu-button {
   box-sizing: border-box;
   height: $h;
@@ -34,18 +53,100 @@ $radius:4px;
   justify-content: center;
   align-items: center;
   white-space: nowrap;
-  background-color: #fff;
+  background: white;
   color: $color;
   border: 1px solid $border-color;
   border-radius: $radius;
-  box-shadow: 0 1px 0 fade-out(black,0.95);
-  outline: none;
-  &+&{
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
+  & + & {
     margin-left: 8px;
   }
-  &:focus,&:hover{
+  &:hover,
+  &:focus {
     color: $blue;
     border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.gulu-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.gulu-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.gulu-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.gulu-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.gulu-theme-button {
+    &.gulu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-link {
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.gulu-theme-text {
+    &.gulu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.gulu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
