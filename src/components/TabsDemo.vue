bfccdc387e7bcad46818d28a7dab436dfc44bd74
siliwu <siliwu@tencent.com>
2021-05-23 19:53:59 +0800

    使用 CSS 切换内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Tabs 示例</h1>
-    <Tabs>
+    <Tabs v-model:selected="x">
       <Tab title="导航1">内容1</Tab>
       <Tab title="导航2">内容2</Tab>
     </Tabs>
@@ -11,9 +11,14 @@
 <script>
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+import { ref } from 'vue';
 export default {
 name: "TabsDemo",
-  components: { Tab, Tabs }
+  components: { Tab, Tabs },
+  setup(){
+    const x= ref('导航1');
+    return {x}
+  }
 }
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c,index) in defaults" :key="index" :is="c"/>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div class="gulu-tabs-nav-item"
+           v-for="(t,index) in titles" :key="index"
+           @click="select(t)"
+           :class="{selected:t===selected}">
+        {{ t }}
+      </div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component v-for="c in defaults" :is="c"
+                 class="gulu-tabs-content-item" :class="{selected:c.props.title===selected}"/>
+    </div>
   </div>
 </template>
 
@@ -10,6 +20,11 @@ import Tab from './Tab.vue';
 
 export default {
   name: 'Tabs',
+  props: {
+    selected: {
+      type: String,
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -18,11 +33,44 @@ export default {
       }
     });
     const titles = defaults.map((tag) => tag.props.title);
-    return {defaults, titles};
+    const select = (title: String) => {
+      context.emit('update:selected', title);
+    };
+    return {defaults, titles, select};
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
 
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item{
+      display: none;
+      &.selected{
+        display: block;
+      }
+    }
+  }
+}
 </style>
