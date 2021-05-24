9a636bbebe54235ddd38150c1bedcecbb621639e
siliwu <siliwu@tencent.com>
2021-05-24 11:56:16 +0800

    实现圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <Topnav/>
     <div>
       <div class="banner">
-        <h1>We UI</h1>
+        <h1>王者 UI</h1>
         <h2>一个厉害的 UI 框架</h2>
         <p class="actions">
           <a href="https://gitee.com/wu-sili" target="_blank">Gitee</a>
@@ -39,6 +39,7 @@ $green: #02bcb0;
 $border-radius: 4px;
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 .banner {
   height: 300px;
